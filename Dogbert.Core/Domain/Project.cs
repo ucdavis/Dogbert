2c52f59e25ad638c923ab352f4a14446bab82460
SU for Alan Lai <srkirkland@gmail.com>
2010-04-27 14:35:12 +0000

    (Updated by TFS to TFS Connector, server: http://1x:8080/, id 5924. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Dogbert/Trunk/Dogbert-MVC;C4657

--- Dogbert.Core/Domain/Project.cs
+++ Dogbert.Core/Domain/Project.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using NHibernate.Validator.Constraints;
 using UCDArch.Core.DomainModel;
@@ -8,78 +8,153 @@ namespace Dogbert.Core.Domain
 {
     public class Project : DomainObject
     {
+        public virtual IList<Requirement> Requirements { get; set; }
+        public virtual IList<ProjectText> ProjectTexts { get; set; }
+        public virtual IList<RequirementCategory> RequirementCategories { get; set; }
+        public virtual IList<UseCase> UseCases { get; set; }
+
         [Required]
         [Length(50)]
         public virtual string Name { get; set; }
-
-        public virtual int? Priority { get; set; }
+        public virtual int Priority { get; set; }
         public virtual DateTime? Deadline { get; set; }
 
+        [Required]
         [Length(100)]
         public virtual string Contact { get; set; }
-
+        [Required]
         [Length(50)]
         public virtual string ContactEmail { get; set; }
-
+        [Required]
         [Length(50)]
         public virtual string Unit { get; set; }
-
-        public virtual int? Complexity { get; set; }
-        public virtual DateTime? ProjectedStart { get; set; }
-        public virtual DateTime? ProjectedEnd { get; set; }
+        
+        public virtual int Complexity { get; set; }
+        public virtual DateTime ProjectedStart { get; set; }
+        public virtual DateTime ProjectedEnd { get; set; }
         public virtual DateTime? BeginDate { get; set; }
         public virtual DateTime? EndDate { get; set; }
+        public virtual DateTime? DateAdded { get; set; }
+        public virtual DateTime? LastModified { get; set; }
+        public virtual StatusCode StatusCode { get; set; }
+        public virtual User ProjectManager { get; set; }
+        public virtual User LeadProgrammer { get; set; }
+        public virtual ProjectType ProjectType { get; set; }
 
-        public virtual string StartDateString
+
+        public virtual string ProjectedStartString
         {
             get
             {
-                if (BeginDate.HasValue)
+                return String.Format("{0:MM/dd/yyyy}", ProjectedStart).ToString();
+            }
+        }
+
+        public virtual string ProjectedEndString
+        {
+            get
+            {
+                return String.Format("{0:MM/dd/yyyy}", ProjectedEnd).ToString();
+            }
+        }
+
+
+        public virtual int? Duration {
+            get{
+                if ((ProjectedStart.ToString() == null) || (ProjectedEnd.ToString() == null))
                 {
+                    return null;
+                }
+                else return ProjectedEnd.Month - ProjectedStart.Month;
+            }
+        }
+
+        public virtual string ProjectManagerString
+        {
+            get
+            {
+                if (ProjectManager == null)
+                {
+                    return ("N/A");
+                }
+                else return ProjectManager.FullName;
+            }
+
+        }
+
+
+        public virtual string LeadProgrammerString
+        {   get
+            {
+                if (LeadProgrammer == null)
+                {
+                    return("N/A");
+                }
+                else return LeadProgrammer.FullName;
+            }
+
+        }
+
+
+        public virtual string StartDateString{
+            get{
+                if (BeginDate.HasValue){
                     return BeginDate.Value.ToShortDateString();
                 }
-
-                if (ProjectedStart.HasValue)
+                if (ProjectedStart.ToString() != null)
                 {
-                    return string.Format("{0} (proj.)", ProjectedStart.Value.ToShortDateString());
+                    return string.Format("{0} (proj.)", ProjectedStart.ToShortDateString());
                 }
-
                 return "N/A";
             }
         }
 
         public virtual string EndDateString
         {
-            get
-            {
-                if (EndDate.HasValue)
-                {
+            get{
+                if (EndDate.HasValue){
                     return EndDate.Value.ToShortDateString();
                 }
-
-                if (ProjectedEnd.HasValue)
+                if (ProjectedEnd.ToString() != null)
                 {
-                    return string.Format("{0} (proj.)", ProjectedEnd.Value.ToShortDateString());
+                    return string.Format("{0} (proj.)", ProjectedEnd.ToShortDateString());
                 }
-
                 return "N/A";
             }
         }
+    
+        public Project()
+        {
+            //create blank list for Ilists.
+            ProjectTexts = new List<ProjectText>();
+            RequirementCategories = new List<RequirementCategory>();
+            Requirements = new List<Requirement>();
+            UseCases = new List<UseCase>();
+        }
 
-        [NotNull]
-        public virtual Status Status { get; set; }
+        public virtual void AddUseCase(UseCase UseCase)
+        {
+            UseCase.Project = this;
+            this.UseCases.Add(UseCase);
+        }
 
-        public virtual User ProjectManager { get; set; }
-        public virtual User LeadProgrammer { get; set; }
+        public virtual void AddRequirement(Requirement Requirement)
+        {
+            Requirement.Project = this;
+            this.Requirements.Add(Requirement);
+        }
 
-        public virtual string Description { get; set; }
-        public virtual string Purpose { get; set; }
+        public virtual void AddRequirementCategory(RequirementCategory RequirementCategory)
+        {
+            RequirementCategory.Project = this;
+            this.RequirementCategories.Add(RequirementCategory);
+        }
 
-        public virtual ProjectType ProjectType { get; set; }
+        public virtual void AddProjectTexts(ProjectText ProjectText)
+        {
+            ProjectText.Project = this;
+            this.ProjectTexts.Add(ProjectText);
+        }
 
-        public virtual IList<Requirement> Requirements { get; set; }
-        public virtual IList<UseCase> UseCases { get; set; }
-        public virtual IList<Category> Categories { get; set; }
-        public virtual IList<ProjectFile> Files { get; set; }
     }
 }
