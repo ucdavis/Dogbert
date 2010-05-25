deb87b9498b815af017ad3051a75754efc7bcb84
Lai, Alan <anlai@ucdavis.edu>
2010-05-25 16:45:10 +0000

    added some columns so that the project file view works correctly.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Dogbert/Trunk/Dogbert-MVC;C7568

--- Dogbert.Core/Domain/TextType.cs
+++ Dogbert.Core/Domain/TextType.cs
@@ -10,6 +10,8 @@ namespace Dogbert.Core.Domain
     {
         public virtual string Name { get; set; }
         public virtual bool IsActive { get; set; }
+        public virtual int Priority { get; set; }
+        public virtual bool HasImage { get; set; }
 
         public virtual void setID(string id)
         {
