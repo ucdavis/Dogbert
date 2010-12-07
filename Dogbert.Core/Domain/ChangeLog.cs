a27c268da530dd2176494d473c6ad374cee81dad
Lai, Alan <anlai@ucdavis.edu>
2010-12-07 16:49:26 +0000

    added change log class
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/Dogbert/Trunk/Dogbert-MVC;C8797

---
+++ Dogbert.Core/Domain/ChangeLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UCDArch.Core.DomainModel;
+
+namespace Dogbert.Core.Domain
+{
+    public class ChangeLog : DomainObject
+    {
+        public ChangeLog()
+        {
+            Created = DateTime.Now;
+            Updated = DateTime.Now;
+        }
+
+        public virtual string Change { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual DateTime Updated { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
